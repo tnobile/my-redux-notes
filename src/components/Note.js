ac67b56d2d3991548991f4362eab15e3127e9197
tnobile <takao.nosaka@outlook.com>
2021-05-16 23:00:22 +0200

    fix issue

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
 import { editNote, deleteNote } from '../redux/actions/actions'
 
-const Note = ({note, dispatch}) => {
+const Note = ({ note, dispatch }) => {
     return (
         <div className="note">
             <h2 className="note_title">{note.title}</h2>
@@ -18,4 +18,5 @@ const Note = ({note, dispatch}) => {
         </div>
     )
 }
+// useDispatch hook will be simpler than Higher Order Component
 export default connect()(Note);
